8440750d7469a5fb2a027cf1ad0d14937228c4af
Pedro Araujo <pharaujo915@gmail.com>
2024-03-20 21:47:49 -0300

    Add Truck and Cargo models in api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Truck(models.Model):
+    truck = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=4)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+class Cargo(models.Model):
+    product = models.CharField(max_length=200)
+    origin_city = models.CharField(max_length=200)
+    origin_state = models.CharField(max_length=4)
+    origin_latitude = models.FloatField()
+    origin_longitude = models.FloatField()
+    destination_city = models.CharField(max_length=200)
+    destination_state = models.CharField(max_length=4)
+    destination_latitude = models.FloatField()
+    destination_longitude = models.FloatField()
