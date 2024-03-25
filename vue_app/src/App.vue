41c7bad00e7be883b82c9e8257cfe384c02b9921
Pedro Araujo <pharaujo915@gmail.com>
2024-03-25 16:41:08 -0300

    Add vue App file

---
+++ vue_app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
