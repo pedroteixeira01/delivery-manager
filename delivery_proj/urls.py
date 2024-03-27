6af26fcaca6805dbc5d66422b62065880cd59ca3
Pedro Araujo <pharaujo915@gmail.com>
2024-03-27 16:42:26 -0300

    Update project URLs

--- delivery_proj/urls.py
+++ delivery_proj/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.urls import router
+from api.views import MappingView
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/mapping/', MappingView),
     path('admin/', admin.site.urls),
 ]
